33b5619d24a0158af1dcb76978534fc29e87b2d8
Pavel <recyclesordie94@gmail.com>
2021-08-16 20:39:58 +0300

    on(), off(), click()

--- readme.md
+++ readme.md
@@ -16,6 +16,24 @@
 ### .addClass(...classNames) - добавить класс\ы
 #### $('.active').addClass('hello', 'world');
 
+### .removeClass(...classNames) - удалить класс\ы
+#### $('.active').removeClass('hello', 'world');
 
+### .toggleClass(...classNames) - переключить класс.
+#### $('.active').toggleClass('hello'); //один аргемент 
+
+
+<hr>
+
+# хендлеры
+
+### .on('eventName', колбэк) - добавить обработчик
+#### сокращение addEventListener('eventName', cb);
+
+### .off('eventName', колбэк) - удалить обработчик
+#### сокращение removeEventListener('eventName', cb);
+
+### .click(handler) - события клика
+#### уже написанное событие блика, которое принимает отдельно написанную функцию-handler
 
 
--- src/js/lib/modules/classes.js
+++ src/js/lib/modules/classes.js
@@ -1,10 +1,25 @@
 import $ from '../core';
 
+$.prototype.addClass = function(...classNames){
+    for (let i = 0; i < this.length; i++) {
+        this[i].classList.add(...classNames);
+    }
 
-$.prototype.addClass = function (...classNames) {
-  for (let i = 0; i < this.length; i++) {
-      this[i].classList.add(...classNames);
-  }  
+    return this;
+};
 
-  return this;
+$.prototype.removeClass = function(...classNames){
+    for (let i = 0; i < this.length; i++) {
+        this[i].classList.remove(...classNames);
+    }
+
+    return this;
+};
+
+$.prototype.toggleClass = function(classNames){
+    for (let i = 0; i < this.length; i++) {
+        this[i].classList.toggle(classNames);
+    }
+
+    return this;
 };
