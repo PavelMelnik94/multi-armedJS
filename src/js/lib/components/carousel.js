905b96781a74158cbe4c48783627752edd1c7487
unknown <recyclesordie94@gmail.com>
2022-01-16 20:18:53 +0300

    feat: add autoplay carousel logic

--- src/js/lib/components/carousel.js
+++ src/js/lib/components/carousel.js
@@ -1,11 +1,23 @@
 import $ from '../core';
 
-$.prototype.carousel = function() {
+$.prototype.carousel = function(settings = {} ) {
+
+    const setup = {
+        isAutoplay: settings.autoplay || false,
+        speed: settings.speed || null,
+        isHoverPause: settings.hoverPause || false,
+        isCircleDots: settings.circleDots || false,
+    }
+
+       
+
     for (let i = 0 ;i < this.length; i++) {
         const width = window.getComputedStyle(this[i].querySelector('.carousel-inner')).width;
         const slides = this[i].querySelectorAll('.carousel-item');
         const slidesField = this[i].querySelector('.carousel-slides');
         const dots = this[i].querySelectorAll('.carousel-indicators li');
+        const prev = this[i].querySelector('.carousel-prev');
+        const next = this[i].querySelector('.carousel-next');
 
         slidesField.style.width = 100 * slides.length + '%';
         slides.forEach(slide => {
@@ -14,6 +26,65 @@ $.prototype.carousel = function() {
 
         let offset = 0;
         let slideIndex = 0;
+
+        const autoPlay = (params = setup) => {
+            if (!params.isAutoplay) {
+                return null;
+            } else if (!params.speed) {
+                throw new Error('Не указана скорость автопрокрутки');
+            } 
+
+            
+            if (params.isAutoplay) {
+                const interval = setInterval(() => {
+                    if (offset == (+width.replace(/\D/g, '') * (slides.length - 1))) {
+                        offset = 0;
+                    } else {
+                        offset += +width.replace(/\D/g, '');
+                    }
+        
+                    slidesField.style.transform = `translateX(-${offset}px)`;
+        
+                    if (slideIndex == slides.length - 1) {
+                        slideIndex = 0;
+                    } else {
+                        slideIndex++;
+                    }
+    
+                    dots.forEach((dot, index) => {
+                        dot.classList.remove('active');
+    
+                        if (index === slideIndex) {
+                            dot.classList.add('active');
+                        }
+                    })
+    
+                    slideIndex++;
+                    //установка скорости из настроек
+                }, params.speed);
+
+
+                
+
+                //stop autoplay on hover
+                if (params.isHoverPause) {
+                    this[i].addEventListener('mouseenter', () => {
+                        console.log('stop autoplay');
+                        clearInterval(interval);
+                    })
+    
+                    this[i].addEventListener('mouseleave', () => {
+                        console.log('start autoplay');
+                        autoPlay(params);
+                    })
+                }
+            }
+        }
+
+        autoPlay();
+        
+        
+
 
         $(this[i].querySelector('[data-slide="next"]')).click((e) => {
             e.preventDefault();
